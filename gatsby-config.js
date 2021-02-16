4eac8d18f49909060c97a40334088d02cc05decb
cieloazul310 <cieloazul310@gmail.com>
2021-02-16 19:46:45 +0900

    #61: update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const baseUrl = 'https://cieloazul310.github.io';
 const pathPrefix = '/rockman';
 const siteUrl = path.join(baseUrl, pathPrefix);
 const contentDir = `${__dirname}/data`;
-//const contentDir = `${__dirname}/devData`;
+// const contentDir = `${__dirname}/devData`;
 
 module.exports = {
   siteMetadata: {
