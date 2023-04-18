33ea37f6d87c1ee26eb584d83366fc8951a50567
cieloazul310 <cieloazul310@gmail.com>
2023-04-18 16:21:11 +0900

    style: fix tonarino tab

--- src/templates/artist/TonarinoTab.tsx
+++ src/templates/artist/TonarinoTab.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Section, SectionWrapper, Article } from '@cieloazul310/gatsby-theme-aoi';
+import { SectionWrapper } from '@cieloazul310/gatsby-theme-aoi';
 import ArtistPageHeader from './PageHeader';
 import { TunesByProgramSkeleton } from '../../components/Tunes/TunesByProgram';
 import type { Artist } from '../../../types';
@@ -14,11 +14,7 @@ function ArtistTonarinoTab({ item }: ArtistTonarinoTabProps) {
   return (
     <SectionWrapper>
       <ArtistPageHeader artist={item} />
-      <Section>
-        <Article maxWidth="md" disableGutters>
-          <TunesByProgramSkeleton />
-        </Article>
-      </Section>
+      <TunesByProgramSkeleton />
     </SectionWrapper>
   );
 }
